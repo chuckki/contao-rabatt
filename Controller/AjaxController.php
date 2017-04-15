07e4dbd124834fe46db2a5aa0b7c0d1b7eff7001
chuckki <chuckki@projektorientiert.de>
2017-04-15 19:24:44 +0200

    add anti-hacking + logger

--- Controller/AjaxController.php
+++ Controller/AjaxController.php
@@ -2,6 +2,7 @@
 
 namespace Chuckki\RabattBundle\Controller;
 
+use Doctrine\ORM\Tools\Console\Command\InfoCommand;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +14,15 @@ class AjaxController extends Controller
      */
     public function indexAction($name)
     {
+    	if(sizeof($name) < 7){
+    		$ipAddress = $_SERVER['REMOTE_ADDR'];
+			if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+				$ipAddress = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
+			}
+    		$this->get('logger')->critical('Rabbat Request < 7',['ip' => $ipAddress, 'request' => $name]);
+			return new Response('Your request is recorded as a hacking attack. Your IP is saved and the admin is noticed. Stop it! Now!');
+		}
+
     	$rabatt = $this->getDoctrine()->getRepository('ChuckkiRabattBundle:Rabatt')
 			->findOneBy(['rabattCode' => $name]);
 
